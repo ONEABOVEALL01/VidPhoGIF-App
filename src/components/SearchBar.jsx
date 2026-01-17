582bb950bfeed455dec67a52c5fe8d4b42dd3b4d
Suman Bera <135948367+ONEABOVEALL01@users.noreply.github.com>
2026-01-18 01:39:37 +0530

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,6 +4,7 @@ import { SetQuery } from "../redux/features/searchSlice";
 import { FaGithub } from "react-icons/fa";
 import logo from "../assets/gif-proj-logo.png";
 
+
 const searchBar = () => {
   const [text, setText] = useState("");
 
@@ -24,11 +25,12 @@ const searchBar = () => {
   return (
     <div className="">
       <div className="flex bg-amber-100 bg-slate-4cd00 h-[8vh] md:h-[10vh] justify-center w-full border shadow-lg border-l-0 border-r-0 border-t-0 border-gray-500 flex-wrap">
-        <div className="h-[3vh] md:h-[7.5vh] w-[9vh] sm:w-[12vh] md:w-[30vh] right-[5vh] relative sm:right-[10vh] md:right-[50vh] top-5.5  md:top-3 cursor-pointer ">
+        <div className="h-[5vh] md:h-[7.5vh] w-[11vh] sm:w-[12vh] md:w-[30vh] right-[7vh] relative sm:right-[10vh] md:right-[50vh] top-4.5  md:top-3 cursor-pointer ">
           <img src={logo} alt="App Logo" />
+
         </div>
-        <div className="  h-[4vh] md:h-[6vh] flex items-center border mt-[2vh] md:mt-[2vh] w-[47vw] sm:w-[50vw] md:w-[63vh] bg-rose-200 border-black rounded-4xl">
-          <form
+        <div className="  h-[4vh] md:h-[6vh] flex  items-center border mt-[2vh] md:mt-[2vh] w-[47vw] sm:w-[50vw] md:w-[63vh] bg-rose-200 border-black rounded-4xl">
+          <form className="flex items-center"
             onSubmit={(e) => {
               controlSubmit(e);
             }}
@@ -44,7 +46,7 @@ const searchBar = () => {
               type="text"
               placeholder="search..."
             />
-            <button className=" active:scale-95 md:w-[10vh] w-[5vh] md:h-[5vh] h-[2.8vh] px-1 sm:px-2 font-semibold  text-center rounded-4xl cursor-pointer ml-[2vh] sm:ml-[4vw] md:ml-[11.5vh] bg-emerald-400 border border-black text-[1.2vh] sm:text-sm md:text-base">
+            <button className=" active:scale-95 md:w-[10vh] w-[5vh] md:h-[4.5vh] h-[2.8vh] px-1  sm:px-2 font-semibold  text-center rounded-4xl cursor-pointer ml-[2vh] sm:ml-[4vw] md:ml-[11.5vh] bg-emerald-400 border border-black text-[1.2vh] sm:text-sm md:text-base">
               Search
             </button>
           </form>
@@ -53,9 +55,9 @@ const searchBar = () => {
 
         </div>
 
-         <div className="relative sm:left-0 top-5 cursor-pointer md:left-[50vh] left-[4.5vh] sm:block">
+         <div className="relative sm:left-0 top-2.5  cursor-pointer md:left-[50vh] left-[7vh] sm:block">
 
-                <FaGithub size={30} onClick={onclick} />
+                <FaGithub className="text-[22px] mt-[1vh] sm:text-[24px] md:text-[30px]" onClick={onclick} />
 
           </div>
 
